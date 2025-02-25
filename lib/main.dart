3e28edfabb888eb978fd894d1d1d9e68c03e9f48
Ahmed Ehab <ahmedehab56200@gmail.com>
2025-02-25 22:08:52 +0200

    refactor P3

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
+import 'package:ai_weather/core/app_router/app_router.dart';
 import 'package:ai_weather/core/di/dependency_injection.dart';
 import 'package:ai_weather/features/auth/presentation/controller/auth_cubit/auth_cubit.dart';
 import 'package:ai_weather/features/home/presentation/controller/weather_cubit/weather_cubit.dart';
-import 'package:ai_weather/features/home/presentation/views/home_view.dart';
 import 'package:ai_weather/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -31,13 +31,9 @@ class AIWeather extends StatelessWidget {
           create: (context) => sl<WeatherCubit>(),
         ),
       ],
-      // child: MaterialApp.router(
-      //   debugShowCheckedModeBanner: false,
-      //   routerConfig: AppRouter.router,
-      // ),
-      child: const MaterialApp(
+      child: MaterialApp.router(
         debugShowCheckedModeBanner: false,
-        home: HomeView(),
+        routerConfig: AppRouter.router,
       ),
     );
   }
